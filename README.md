ebd693033e0bc65900aab4be57358b54d19852e0
Loïc Knuchel <loicknuchel@gmail.com>
2016-10-09 11:50:58 +0200

    improve embed with Reveal

--- README.md
+++ README.md
@@ -1,5 +1,12 @@
 # Backend for HumanTalks organizers
 
+## TODO
+
+- 107 vidéos enregistrés alors qu'il y en a 120 sur youtube... (voir celles qui manquent)
+- rentrer l'historique de humantalks.com
+- supprimer les personnes en double
+- enrichir le profil des personnes (twitter et mail) et des lieux (contact et places)
+
 ## Deploy
 
 When packages changes, heroku may be lost.
--- app/com/humantalks/common/services/EmbedSrv.scala
+++ app/com/humantalks/common/services/EmbedSrv.scala
@@ -2,7 +2,7 @@ package com.humantalks.common.services
 
 import global.models.ApiError
 import play.api.libs.json.Json
-import play.api.libs.ws.WSClient
+import play.api.libs.ws.{ WSResponse, WSClient }
 
 import scala.collection.mutable
 import scala.concurrent.duration._
@@ -38,11 +38,18 @@ object EmbedSrv {
     case _ => default
   }
 
-  def embedRemote(ws: WSClient)(url: String, default: Either[ApiError, EmbedData] = default)(implicit ec: ExecutionContext): Future[Either[ApiError, EmbedData]] = url match {
-    case SlideShare.url(user, slidesId) => SlideShare.embed(ws)(url)
-    case SpeakerDeck.url(user, slidesId) => SpeakerDeck.embed(ws)(url)
-    case _ => Future(embed(url, default))
-  }
+  def embedRemote(ws: WSClient)(url: String, default: Either[ApiError, EmbedData] = default)(implicit ec: ExecutionContext): Future[Either[ApiError, EmbedData]] =
+    embed(url) match {
+      case Right(embedData) => Future(Right(embedData))
+      case Left(err) => fetch(ws)(url) {
+        _.body match {
+          case SlideShare.isPresent(embedUrl) if SlideShare.url.findFirstIn(url).isDefined => Right(SlideShare.embed(url, EmbedUrl(embedUrl)))
+          case SpeakerDeck.isPresent(embedId, embedRatio) if SpeakerDeck.url.findFirstIn(url).isDefined => Right(SpeakerDeck.embed(url, embedId, embedRatio))
+          case Reveal.isPresent() => Right(Reveal.embed(url))
+          case _ => default
+        }
+      }
+    }
 
   private val timeout = 10000.millis
   private val resolveCache = mutable.WeakHashMap.empty[String, String]
@@ -113,42 +120,35 @@ object EmbedSrv {
     def embedCode(embedUrl: EmbedUrl): EmbedCode = EmbedCode(s"""<iframe src="$embedUrl" width="576" height="420" scrolling="no" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>""")
     def embed(url: String, user: String, slidesId: String): EmbedData = EmbedData(url, name, embedUrl(user, slidesId).value, embedCode(embedUrl(user, slidesId)).value)
   }
-  private object SlideShare {
-    val name = "SlideShare"
-    val url = s"https?://[a-z]+.slideshare.net/$hash/$hash.*".r
-    private val embedUrlRegex = "(?is).*<meta class=\"twitter_player\" value=\"([^\"]+)\" name=\"twitter:player\" />.*".r
-    def embedUrl(ws: WSClient)(url: String)(implicit ec: ExecutionContext): Future[Either[ApiError, EmbedUrl]] = ws.url(url).withRequestTimeout(timeout).get().map {
-      _.body match {
-        case embedUrlRegex(u) => Right(EmbedUrl(u))
-        case _ => Left(ApiError.notFound(s"Embed data not found in $name page"))
-      }
-    }.recover {
-      case e: Throwable => Left(ApiError.from(e))
-    }
-    def embedCode(embedUrl: EmbedUrl): EmbedCode = EmbedCode(s"""<iframe src="$embedUrl" width="595" height="485" frameborder="0" marginwidth="0" marginheight="0" scrolling="no" style="border:1px solid #CCC; border-width:1px; margin-bottom:5px; max-width: 100%;" allowfullscreen></iframe>""")
-    def embed(url: String, embedUrl: EmbedUrl): EmbedData = EmbedData(url, name, embedUrl.value, embedCode(embedUrl).value)
-    def embed(ws: WSClient)(url: String)(implicit ec: ExecutionContext): Future[Either[ApiError, EmbedData]] = embedUrl(ws)(url).map { _.right.map { embedUrl => embed(url, embedUrl) } }
-  }
-  private object SpeakerDeck {
-    val name = "SpeakerDeck"
-    val url = s"https?://speakerdeck.com/$hash/$hash.*".r
-    private val embedUrlRegex = "(?is).*<div class=\"speakerdeck-embed\" data-id=\"([^\"]+)\" data-ratio=\"([^\"]+)\"></div>.*".r
-    def embedUrl(ws: WSClient)(url: String)(implicit ec: ExecutionContext): Future[Either[ApiError, (String, String)]] = ws.url(url).withRequestTimeout(timeout).get().map {
-      _.body match {
-        case embedUrlRegex(embedId, embedRatio) => Right((embedId, embedRatio))
-        case _ => Left(ApiError.notFound(s"Embed data not found in $name page"))
-      }
-    }.recover {
-      case e: Throwable => Left(ApiError.from(e))
-    }
-    def embedCode(embedId: String, embedRatio: String): EmbedCode = EmbedCode(s"""<script async class="speakerdeck-embed" data-id="$embedId" data-ratio="$embedRatio" src="//speakerdeck.com/assets/embed.js"></script>""")
-    def embed(url: String, embedId: String, embedRatio: String): EmbedData = EmbedData(url, name, url, embedCode(embedId, embedRatio).value)
-    def embed(ws: WSClient)(url: String)(implicit ec: ExecutionContext): Future[Either[ApiError, EmbedData]] = embedUrl(ws)(url).map { _.right.map { case (embedId, embedRatio) => embed(url, embedId, embedRatio) } }
-  }
   private object Pdf {
     val name = "Pdf"
     val url = s".*\\.pdf".r
     def embedCode(url: String): EmbedCode = EmbedCode(s"""<object data="$url" type="application/pdf" width="640" height="480">alt : <a href="$url">$url</a></object>""")
     def embed(url: String): EmbedData = EmbedData(url, name, url, embedCode(url).value)
   }
+  private object SlideShare {
+    val name = "SlideShare"
+    val url = s"https?://[a-z]+.slideshare.net/$hash/$hash.*".r
+    val isPresent = "(?is).*<meta class=\"twitter_player\" value=\"([^\"]+)\" name=\"twitter:player\" />.*".r
+    def embedCode(embedUrl: EmbedUrl): EmbedCode = EmbedCode(s"""<iframe src="$embedUrl" width="595" height="485" frameborder="0" marginwidth="0" marginheight="0" scrolling="no" style="border:1px solid #CCC; border-width:1px; margin-bottom:5px; max-width: 100%;" allowfullscreen></iframe>""")
+    def embed(url: String, embedUrl: EmbedUrl): EmbedData = EmbedData(url, name, embedUrl.value, embedCode(embedUrl).value)
+  }
+  private object SpeakerDeck {
+    val name = "SpeakerDeck"
+    val url = s"https?://speakerdeck.com/$hash/$hash.*".r
+    val isPresent = "(?is).*<div class=\"speakerdeck-embed\" data-id=\"([^\"]+)\" data-ratio=\"([^\"]+)\"></div>.*".r
+    def embedCode(embedId: String, embedRatio: String): EmbedCode = EmbedCode(s"""<script async class="speakerdeck-embed" data-id="$embedId" data-ratio="$embedRatio" src="//speakerdeck.com/assets/embed.js"></script>""")
+    def embed(url: String, embedId: String, embedRatio: String): EmbedData = EmbedData(url, name, url, embedCode(embedId, embedRatio).value)
+  }
+  private object Reveal {
+    val name = "Reveal"
+    val isPresent = "(?is).*<div class=\"reveal.*".r
+    def embedCode(url: String): EmbedCode = EmbedCode(s"""<iframe src="$url" width="595" height="485" frameborder="0"></iframe>""")
+    def embed(url: String): EmbedData = EmbedData(url, name, url, embedCode(url).value)
+  }
+
+  private def fetch[T](ws: WSClient)(url: String)(transform: WSResponse => Either[ApiError, T])(implicit ec: ExecutionContext): Future[Either[ApiError, T]] =
+    ws.url(url).withRequestTimeout(timeout).get().map(transform).recover {
+      case e: Throwable => Left(ApiError.from(e))
+    }
 }
