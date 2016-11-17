28cf63715e94ed5bc811afb615232e54c9ca8c62
Loïc Knuchel <loicknuchel@gmail.com>
2016-11-17 01:22:00 +0100

    add mustache templating for config

---
+++ app/com/humantalks/common/services/MustacheSrv.scala
@@ -0,0 +1,29 @@
+package com.humantalks.common.services
+
+import java.io.{ StringReader, StringWriter }
+import play.api.libs.json._
+
+import scala.collection.JavaConverters._
+
+import com.github.mustachejava.DefaultMustacheFactory
+
+object MustacheSrv {
+  def build(template: String, scopes: Map[String, JsValue]): String = {
+    val writer = new StringWriter()
+    val mustacheFactory = new DefaultMustacheFactory()
+    val mustache = mustacheFactory.compile(new StringReader(template), "dummyFileName")
+    mustache.execute(writer, scopes.filter(_._2 != JsNull).map { case (key, value) => (key, format(value)) }.asJava)
+    writer.toString.trim
+  }
+
+  private def format(json: JsValue): Object = {
+    (json match {
+      case JsObject(obj) => obj.map { case (key, value) => (key, format(value)) }.asJava
+      case JsArray(arr) => arr.map(format).asJava
+      case JsString(str) => str
+      case JsNumber(n) => n
+      case JsBoolean(b) => b
+      case JsNull => null
+    }).asInstanceOf[Object]
+  }
+}
--- app/com/humantalks/internal/admin/config/ConfigApiCtrl.scala
+++ app/com/humantalks/internal/admin/config/ConfigApiCtrl.scala
@@ -2,9 +2,11 @@ package com.humantalks.internal.admin.config
 
 import com.humantalks.auth.authorizations.WithRole
 import com.humantalks.auth.silhouette.SilhouetteEnv
+import com.humantalks.common.services.MustacheSrv
+import com.humantalks.exposed.entities.{ PublicVenue, PublicMeetup, PublicTalk }
 import com.humantalks.internal.meetups.{ Meetup, MeetupDbService }
 import com.humantalks.internal.persons.{ PersonDbService, Person }
-import com.humantalks.internal.talks.TalkDbService
+import com.humantalks.internal.talks.{ Talk, TalkDbService }
 import com.humantalks.internal.venues.{ Venue, VenueDbService }
 import com.mohiva.play.silhouette.api.Silhouette
 import global.Contexts
@@ -34,18 +36,18 @@ case class ConfigApiCtrl(
       case "meetup.event.description" => for {
         meetupOpt <- (req.body \ "id").asOpt[String].flatMap(id => Meetup.Id.from(id).right.toOption).map(id => meetupDbService.get(id)).getOrElse(meetupDbService.getLast)
         venueOpt <- meetupOpt.flatMap(_.data.venue).map(venueDbService.get).getOrElse(Future.successful(None))
-      } yield Ok(meetupDescTemplate(value, meetupOpt, venueOpt))
+        talks <- meetupOpt.map(meetup => talkDbService.findByIds(meetup.data.talks)).getOrElse(Future.successful(List()))
+        speakers <- personDbService.findByIds(talks.flatMap(_.data.speakers))
+      } yield Ok(meetupDescTemplate(value, meetupOpt, venueOpt, talks, speakers))
       case _ => Future.successful(NotFound(s"No preview defined for ref '$ref'"))
     }).getOrElse(Future.successful(BadRequest(s"Incorrect payload format :\n\n${Json.prettyPrint(req.body)}\n\nAttributes 'ref' & 'value' are required !")))
   }
 
-  private def meetupDescTemplate(template: String, meetupOpt: Option[Meetup], venueOpt: Option[Venue]): String = {
-    val withMeetup = meetupOpt.map { meetup =>
-      template.replaceAll("\\{\\{meetup.title}}", meetup.data.title).replaceAll("\\{\\{meetup.description}}", meetup.data.description.getOrElse(""))
-    }.getOrElse(template)
-    val withVenue = venueOpt.map { venue =>
-      withMeetup.replaceAll("\\{\\{venue.name}}", venue.data.name).replaceAll("\\{\\{venue.logo}}", venue.data.logo.getOrElse(""))
-    }.getOrElse(withMeetup)
-    withVenue.replaceAll("\n\n\n\n", "\n\n").trim
+  private def meetupDescTemplate(template: String, meetupOpt: Option[Meetup], venueOpt: Option[Venue], talks: List[Talk], speakers: List[Person]): String = {
+    MustacheSrv.build(template, Map(
+      "meetup" -> Json.toJson(meetupOpt.map(m => PublicMeetup.from(m, None, None, None))),
+      "venue" -> Json.toJson(venueOpt.map(v => PublicVenue.from(v, None, None, None))),
+      "talks" -> Json.toJson(talks.map(t => PublicTalk.from(t, Some(speakers), None, None)))
+    ))
   }
 }
