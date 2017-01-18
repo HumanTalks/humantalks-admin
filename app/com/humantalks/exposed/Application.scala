45a5fb31aa450a6d7e143bd09d3c2d8b34ed3117
Loïc Knuchel <loicknuchel@gmail.com>
2017-01-18 01:26:39 +0100

    remove public talks

--- app/com/humantalks/exposed/Application.scala
+++ app/com/humantalks/exposed/Application.scala
@@ -1,16 +1,12 @@
 package com.humantalks.exposed
 
-import controllers.Assets
 import global.Contexts
 import play.api.mvc.{ Action, Controller }
 
 case class Application(
     ctx: Contexts
 ) extends Controller {
-  import Contexts.wsToEC
-  import ctx._
-
-  def index = Action.async { implicit req =>
-    Assets.at("/public", "index.html").apply(req)
+  def index = Action { implicit req =>
+    Ok(views.html.index())
   }
 }
